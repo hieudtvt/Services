0dfa80b473c4c834b3a4faf4a55961852a1c3d3a
hieudtvt <115527003+hieudtvt@users.noreply.github.com>
2022-10-20 21:49:10 +0700

    Cài đặt nginx bằng script

---
+++ install_nginx.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+sudo yum install -y yum-utils
+
+cat > /etc/yum.repos.d/nginx.repo << "EOF"
+[nginx-stable]
+name=nginx stable repo
+baseurl=http://nginx.org/packages/centos/$releasever/$basearch/
+gpgcheck=1
+enabled=1
+gpgkey=https://nginx.org/keys/nginx_signing.key
+module_hotfixes=true
+EOF
+
+yum install -y nginx
+
+systemctl start nginx
+systemctl enabled nginx
+
+firewall-cmd --zone=public --add-port=80/tcp --permanent 
+firewall-cmd --reload
